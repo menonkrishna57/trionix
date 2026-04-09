1c611789bd1d977d1e3259b021215e75b21a91f6
Krishna Menon <menonkrishna57@gmail.com>
2026-04-09 18:44:41 +0530

    added YouTube downloader script and update Dockerfile to include yt-dlp

--- project_6_trionix/python_scripts/youtube_downv3.py
+++ project_6_trionix/python_scripts/youtube_downv3.py
@@ -1,31 +1,43 @@
-from pytubefix import YouTube
+import yt_dlp
 import os
-# TODO: add audio download
+
 def download_youtube_audio(link):
-    yt = YouTube(link, 'WEB')
-    print(yt.title)
-    mypath=os.path.join(os.getcwd(),"project_6_trionix","data")
-    audiopath=os.path.join(mypath,f"{yt.title}.mp3")
-    ys = yt.streams.filter(only_audio=True).first()
-    if not os.path.exists(audiopath) or (os.path.exists(audiopath) and os.path.getsize(audiopath) != ys.filesize):
-        print("Downloading audio...")
-        ys.download(mypath, filename=f"{yt.title}.mp3")
+    mypath = os.path.join(os.getcwd(), "project_6_trionix", "data")
+    os.makedirs(mypath, exist_ok=True)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': os.path.join(mypath, '%(title)s.%(ext)s'),
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(link, download=True)
+        # Use prepare_filename to get the actual sanitized filename yt-dlp used
+        filepath = ydl.prepare_filename(info)
+        # Change extension to .mp3 since we postprocess to mp3
+        audiopath = os.path.splitext(filepath)[0] + '.mp3'
         print("Download completed!")
-    else:
-        print("File already exists!")
-    return os.path.join(audiopath)
+        return audiopath
+
 def download_youtube_video(link):
-    yt = YouTube(link, 'WEB')
-    print(yt.title)
-    mypath=os.path.join(os.getcwd(),"project_6_trionix","data")
-    vidpath=os.path.join(mypath,f"{yt.title}.mp4")
-    ys = yt.streams.get_highest_resolution()
-    if not os.path.exists(vidpath) or (os.path.exists(vidpath) and os.path.getsize(vidpath) != ys.filesize):
-        print("Downloading...")
-        ys.download(mypath)
+    mypath = os.path.join(os.getcwd(), "project_6_trionix", "data")
+    os.makedirs(mypath, exist_ok=True)
+
+    ydl_opts = {
+        'format': 'best[ext=mp4]/best',
+        'outtmpl': os.path.join(mypath, '%(title)s.%(ext)s'),
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(link, download=True)
+        # Use prepare_filename to get the actual sanitized filename yt-dlp used
+        vidpath = ydl.prepare_filename(info)
         print("Download completed!")
-    else:
-        print("File already exists!")
-    return str(os.path.join(vidpath))
+        return str(vidpath)
 
 # print(download_youtube_video("https://youtu.be/9ngnCrBKWZA?si=rEuk5WUIk-csKFNm"))
